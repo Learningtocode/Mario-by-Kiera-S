1995ebcb79d074adcea7e5d8d05f3f4f5e0b0913
Kiera <JustForLaughts66@gmail.com>
2014-11-21 09:21:00 -0800

    On title screen...

--- public_html/js/screens/title.js
+++ public_html/js/screens/title.js
@@ -3,7 +3,7 @@ game.TitleScreen = me.ScreenObject.extend({
 	 *  action to perform on state change
 	 */
 	onResetEvent: function() {	
-		; // TODO
+		var titleImage = new me.Sprite(0, 0, me.loader.getImage("title-screen"));
 	},
 	
 	
